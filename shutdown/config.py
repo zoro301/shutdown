979fe948693fbcd620b1e1bda1dd0d493d54a9d5
zoro301 <zororq@gmail.com>
2014-12-25 09:57:23 +0800

    Update config.py
    comment

--- shutdown/config.py
+++ shutdown/config.py
@@ -1,19 +1,25 @@
 #coding: utf-8
 
-POP3_HOST='pop.qq.com'
-MAIL_USERNAME='357755198@qq.com'
-MAIL_PASSWORD='passw0rd'
+######################################
+#receive email
+######################################
+POP3_HOST='pop.***.com'
+MAIL_USERNAME='********@******.com'
+MAIL_PASSWORD='********'
 SUBJECT='Subject: shutdown'
 
+######################################
+#send email
+######################################
 EMAIL_SERVER = {
-    "server": "smtp.163.com",
-	"user": "renqiang_server@163.com",
-	"password": "passw0rd",
+    "server": "smtp.********.com",
+	"user": "************@***.com",
+	"password": "**********",
 	"port":465,
 }
 
 REPLY_SUBJECT = '关机通知类邮件'
 
-RECEVIE_EMAIL = ['357755198@qq.com, ']
+RECEVIE_EMAIL = ['**************@***.com, ']
 
-FROM_EMAIL = 'RENQIANG <renqiang_server@163.com>'
+FROM_EMAIL = 'RENQIANG-NO-REPLY <*******@****.com>'
